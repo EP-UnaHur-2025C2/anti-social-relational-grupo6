3c317ec9f835813a097116c427c4ec7e14aad6a9
hugoemanuel <ifranluzbelito@gmail.com>
2025-10-20 17:40:57 -0300

    fix en seed followes

--- db/seeders/20251018181601-demo-followes.js
+++ db/seeders/20251018181601-demo-followes.js
@@ -5,89 +5,99 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Followers', [
-      // alice_wonder sigue a bob, charlie y diana
       {
         follower_nickname: 'alice_wonder',
         followed_nickname: 'bob_builder',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'alice_wonder',
         followed_nickname: 'charlie_brown',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'alice_wonder',
         followed_nickname: 'diana_prince',
-  
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
 
       // bob_builder sigue a alice y diana
       {
         follower_nickname: 'bob_builder',
         followed_nickname: 'alice_wonder',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'bob_builder',
         followed_nickname: 'diana_prince',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
-
-      // charlie_brown sigue a todos menos a él mismo
       {
         follower_nickname: 'charlie_brown',
         followed_nickname: 'alice_wonder',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'charlie_brown',
         followed_nickname: 'bob_builder',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'charlie_brown',
         followed_nickname: 'diana_prince',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'charlie_brown',
         followed_nickname: 'edward_stark',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
 
       // diana_prince sigue a bob y edward
       {
         follower_nickname: 'diana_prince',
         followed_nickname: 'bob_builder',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'diana_prince',
         followed_nickname: 'edward_stark',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
-
-      // edward_stark sigue a todos menos a él mismo
       {
         follower_nickname: 'edward_stark',
         followed_nickname: 'alice_wonder',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'edward_stark',
         followed_nickname: 'bob_builder',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'edward_stark',
         followed_nickname: 'charlie_brown',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       },
       {
         follower_nickname: 'edward_stark',
         followed_nickname: 'diana_prince',
-
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ], {});
   },
